4728aa584274ab02e8b26c487755fbd3ded5fde0
Javier Gomez <javier@tagshelf.com>
2020-09-04 19:21:43 -0400

    Fetchfunction (#4)
    * defined function
    
    * button calls fetch function and renders result
    
    * updated pandas requirements
    
    * cleaned up unused static files

--- project/app/views.py
+++ project/app/views.py
@@ -4,8 +4,29 @@ from django.template import loader
 # Create your views here.
 
 def index(request):
-    context = {
-    }
+    context = {}
+
+    if request.method == 'POST' and 'run_script' in request.POST:
+        # import function to run
+        from .fetch import fetch
+        import pandas as pd
+
+        # Endpoint to fetch data from
+        url = 'https://api.usaspending.gov/api/v2/references/toptier_agencies/'
+
+        # Call function
+        r = fetch(url)
+        results = r["results"]
+
+        # Turn results into a DataFrame and then to HTML
+        results = pd.DataFrame(results)
+        table = results.to_html(index=False)
+        
+        # Return data on same page
+        context = {
+        "table":table,
+        }
+        return render(request, 'app/index.html', context)
 
     return render(request, 'app/index.html', context)
 
