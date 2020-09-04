7066dae45dc062f77bcbfea603c7a64476aab724
Javier Gomez <javier@tagshelf.com>
2020-09-04 18:10:52 -0400

    Setupfrontend (#2)
    * create base url
    
    * setup initial view

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.template import loader
 # Create your views here.
+
+def index(request):
+    context = {
+    }
+
+    return render(request, 'app/index.html', context)
+
