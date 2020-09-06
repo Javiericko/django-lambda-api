098a867f8802107e7dae7255b29610ca603340c9
Javier Gomez <javier@tagshelf.com>
2020-09-06 01:51:34 -0400

    Tablestyles (#7)
    * formatted displayed data from view
    
    * added jQuery datatable
    
    * added data source reference
    
    * modified table features
    
    * set label text color to black
    
    * moved fetch command to separate folder
    
    * switched bootstrap styling
    
    * formatted column names
    
    * moved js to end of index html file
    
    * moved code out of views file
    
    * removed header text wrapping
    
    * adjusted scrollY
    
    * updated requirements
    
    * removed debug comments
    
    * updated padding around table features

--- project/app/fetch.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-# import function to run
-def fetch(url):
-    r = requests.get(url).json()
-    return r
--- project/app/views.py
+++ project/app/views.py
@@ -6,25 +6,18 @@ from django.template import loader
 def index(request):
     context = {}
 
-    if request.method == 'POST' and 'run_script' in request.POST:
-        # import function to run
-        from .fetch import fetch
-        import pandas as pd
-
-        # Endpoint to fetch data from
-        url = 'https://api.usaspending.gov/api/v2/references/toptier_agencies/'
-
-        # Call function
-        r = fetch(url)
-        results = r["results"]
-
-        # Turn results into a DataFrame and then to HTML
-        results = pd.DataFrame(results)
-        table = results.to_html(index=False)
+    if request.method == "POST" and "run_script" in request.POST: #using post here to avoid csrf display
         
+        # Import function to run
+        from .commands.fetch import fetch
+
+        # Call function to get results from the API
+        source, table = fetch()
+
         # Return data on same page
         context = {
         "table":table,
+        "source":source
         }
         return render(request, 'app/index.html', context)
 
